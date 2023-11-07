37e6d5e7a7eec5d97cb41f5635a9992a9012bb4b
Sara Mahmoud <saramahmoud5005@gmail.com>
2023-11-07 11:56:36 +0300

    messenger screen updated with ListView

--- lib/messenger_screen.dart
+++ lib/messenger_screen.dart
@@ -60,378 +60,169 @@ class MessengerScreen extends StatelessWidget{
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  10.0,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10.0,
+                  ),
+                  color: Colors.grey[300],
                 ),
-                color: Colors.grey[300],
-              ),
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.search,
-                  ),
-                  SizedBox(
-                    width: 15.0,
-                  ),
-                  Text(
-                    'Search'
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    width: 60.0,
-                    child: Column(
-                      children: [
-                        Stack(
-                          alignment: AlignmentDirectional.bottomEnd,
-                          children: [
-                            CircleAvatar(
-                              radius: 30.0,
-                              backgroundColor: Colors.blue,
-                            ),
-
-                            Padding(
-                              padding: const EdgeInsetsDirectional.only(
-                                bottom: 3.0,
-                                end: 3.0,
-                              ),
-                              child: CircleAvatar(
-                                radius: 8.0,
-                                backgroundColor: Colors.green,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          'Sara Mahmoud Kamal',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
                     ),
-                  ),
-                  SizedBox(
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      'Search'
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                    itemBuilder: (context,index) => buildStoryItem(),
+                  separatorBuilder: (context,index) => SizedBox(
                     width: 20.0,
                   ),
-                  Container(
-                    width: 60.0,
-                    child: Column(
-                      children: [
-                        Stack(
-                          alignment: AlignmentDirectional.bottomEnd,
-                          children: [
-                            CircleAvatar(
-                              radius: 30.0,
-                              backgroundColor: Colors.blue,
-                            ),
-
-                            Padding(
-                              padding: const EdgeInsetsDirectional.only(
-                                bottom: 3.0,
-                                end: 3.0,
-                              ),
-                              child: CircleAvatar(
-                                radius: 8.0,
-                                backgroundColor: Colors.green,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          'Sara Mahmoud Kamal',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Container(
-                    width: 60.0,
-                    child: Column(
-                      children: [
-                        Stack(
-                          alignment: AlignmentDirectional.bottomEnd,
-                          children: [
-                            CircleAvatar(
-                              radius: 30.0,
-                              backgroundColor: Colors.blue,
-                            ),
-
-                            Padding(
-                              padding: const EdgeInsetsDirectional.only(
-                                bottom: 3.0,
-                                end: 3.0,
-                              ),
-                              child: CircleAvatar(
-                                radius: 8.0,
-                                backgroundColor: Colors.green,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          'Sara Mahmoud Kamal',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  itemCount: 10,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            SingleChildScrollView(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Stack(
-                        alignment: AlignmentDirectional.bottomEnd,
-                        children: [
-                          CircleAvatar(
-                            radius: 30.0,
-                            backgroundColor: Colors.blue,
-                          ),
-
-                          Padding(
-                            padding: const EdgeInsetsDirectional.only(
-                              bottom: 3.0,
-                              end: 3.0,
-                            ),
-                            child: CircleAvatar(
-                              radius: 8.0,
-                              backgroundColor: Colors.green,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Sara Mahmoud',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    'contains massege here contains massege here contains massege here contains massege here',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                                  child: Container(
-                                    width: 7,
-                                    height: 7,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blue,
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                Text('02:00 pm'),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Row(
-                    children: [
-                      Stack(
-                        alignment: AlignmentDirectional.bottomEnd,
-                        children: [
-                          CircleAvatar(
-                            radius: 30.0,
-                            backgroundColor: Colors.blue,
-                          ),
-
-                          Padding(
-                            padding: const EdgeInsetsDirectional.only(
-                              bottom: 3.0,
-                              end: 3.0,
-                            ),
-                            child: CircleAvatar(
-                              radius: 8.0,
-                              backgroundColor: Colors.green,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Sara Mahmoud',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    'contains massege here contains massege here contains massege here contains massege here',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                                  child: Container(
-                                    width: 7,
-                                    height: 7,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blue,
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                Text('02:00 pm'),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Row(
-                    children: [
-                      Stack(
-                        alignment: AlignmentDirectional.bottomEnd,
-                        children: [
-                          CircleAvatar(
-                            radius: 30.0,
-                            backgroundColor: Colors.blue,
-                          ),
-
-                          Padding(
-                            padding: const EdgeInsetsDirectional.only(
-                              bottom: 3.0,
-                              end: 3.0,
-                            ),
-                            child: CircleAvatar(
-                              radius: 8.0,
-                              backgroundColor: Colors.green,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Sara Mahmoud',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    'contains massege here contains massege here contains massege here contains massege here',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                                  child: Container(
-                                    width: 7,
-                                    height: 7,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blue,
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                Text('02:00 pm'),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+              SizedBox(
+                height: 40.0,
               ),
-            ),
-          ],
+              ListView.separated(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                  itemBuilder: (context,index) => buildChatItem(),
+                  separatorBuilder: (context,index) => SizedBox(
+                    height: 20.0,
+                  ),
+                  itemCount: 15,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget buildChatItem() => Row(
+    children: [
+      Stack(
+        alignment: AlignmentDirectional.bottomEnd,
+        children: [
+          CircleAvatar(
+            radius: 30.0,
+            backgroundColor: Colors.blue,
+          ),
+
+          Padding(
+            padding: const EdgeInsetsDirectional.only(
+              bottom: 3.0,
+              end: 3.0,
+            ),
+            child: CircleAvatar(
+              radius: 8.0,
+              backgroundColor: Colors.green,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        width: 10.0,
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Sara Mahmoud',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'contains massege here contains massege here contains massege here contains massege here',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    width: 7,
+                    height: 7,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Text('02:00 pm'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+  Widget buildStoryItem() => Container(
+    width: 60.0,
+    child: Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.bottomEnd,
+          children: [
+            CircleAvatar(
+              radius: 30.0,
+              backgroundColor: Colors.blue,
+            ),
+
+            Padding(
+              padding: const EdgeInsetsDirectional.only(
+                bottom: 3.0,
+                end: 3.0,
+              ),
+              child: CircleAvatar(
+                radius: 8.0,
+                backgroundColor: Colors.green,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          'Sara Mahmoud Kamal',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
+    ),
+  );
 
 }
